1ddc48e2f073735136436623f464cf54de2d52ae
Marc Christian Encarnacion <91091791@g.mvusd.net>
2021-03-01 19:19:45 +0000

    Initial commit

---
+++ JTabbedPaneFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class JTabbedPaneFrame extends JFrame
+{
+  public JTabbedPaneFrame()
+  {
+    super("JTabbedPaneFrame Demo");
+
+  }//end default and only constructor
+}//end class
---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main
+{
+  public static void main(String args[])
+  {
+    JTabbedPaneFrame tpf = new JTabbedPaneFrame();//tpf = tabbed pane frame
+    tpf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    tpf.setSize(450, 400);//this size is up to you 
+    tpf.setVisible(true);
+    
+    
+  }//end main method
+}//end Main class
