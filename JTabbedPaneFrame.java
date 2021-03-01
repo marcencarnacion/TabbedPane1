9bbbffbf35c3186db9ed879d61ffed4262325ecd
Marc Christian Encarnacion <91091791@g.mvusd.net>
2021-03-01 19:52:37 +0000

    Added several tabs with buttons on tab three

--- JTabbedPaneFrame.java
+++ JTabbedPaneFrame.java
@@ -1,9 +1,49 @@
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
 public class JTabbedPaneFrame extends JFrame
 {
   public JTabbedPaneFrame()
   {
     super("JTabbedPaneFrame Demo");
 
+    //create the tabbed pane/ holds tab formatting
+    JTabbedPane tabPane = new JTabbedPane();
+
+    //add first tab
+    JLabel label1 = new JLabel ("This is the big area called panel one", SwingConstants.CENTER);
+    JPanel panel1 = new JPanel();
+    panel1.add(label1);
+    tabPane.addTab("Tab One", null, panel1, "FirstPanel");
+
+    //add second tab
+    JLabel label2 = new JLabel ("You can add other stuff to this tabbed panel", SwingConstants.CENTER);
+    JPanel panel2 = new JPanel();
+    panel2.add(label2);
+    tabPane.addTab("Tab Two", null, panel2, "SecondPanel");
+
+    //add third tab
+    JLabel label3 = new JLabel ("These are fabulous buttons here", SwingConstants.CENTER);
+    JPanel panel3 = new JPanel();
+    panel3.setLayout(new BorderLayout());
+    panel3.add(label3);
+    panel3.add(new JButton("North"), BorderLayout.NORTH);
+    panel3.add(new JButton("South"), BorderLayout.SOUTH);
+    panel3.add(new JButton("West"), BorderLayout.WEST);
+    panel3.add(new JButton("East"), BorderLayout.EAST);
+    tabPane.addTab("Tab Three", null, panel3, "ThirdPanel");//keep the add below al the tab's formatting
+
+  
+
+
+    //keep this at the bottom
+    add(tabPane);
+
   }//end default and only constructor
+
 }//end class
